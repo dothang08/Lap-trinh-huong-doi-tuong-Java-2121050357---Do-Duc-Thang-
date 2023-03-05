466fb2993287a6dbd0c8b34a1262d508c4dfbe3f
dothang08 <125253256+dothang08@users.noreply.github.com>
2023-03-06 06:59:55 +0700

    Add files via upload

---
+++ BT Tuần 4/BT_2.java
@@ -0,0 +1,20 @@
+package BT_2;
+
+import java.util.Scanner;
+
+public class BT_2 {
+	
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap 1 so bat ki: ");
+		n = sc.nextInt();
+		if(n % 2 ==0 ) { 
+			System.out.println( n + " la so chan");
+		} else {
+			System.out.println(n + " la so le");
+		}
+		
+	}
+	//Code by Do Thang
+}
